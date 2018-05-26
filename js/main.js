a06e6c6c35262924947c131b7c1aed93fe72e0e7
Akshay Shah <akshay.shah@knappily.com>
2018-05-26 14:46:56 -0400

    v2.1

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ function createTeamInFoElement(teamAPIResponse) {
         let team_name = teamAPIResponse[i].teamName;
         let team_url;
         if (team_name === 'NDG Strikers') {
+            team_name  = 'NDG' + '<br>' + 'Strikers';
             team_url = 'https://webmountstudio.com/apl_montreal/NDG';
         }
         if (team_name === 'Mont-Royal Eagles') {
@@ -20,6 +21,7 @@ function createTeamInFoElement(teamAPIResponse) {
             team_url = 'https://webmountstudio.com/apl_montreal/SS';
         }
         if (team_name === 'Laval Titans') {
+            team_name = 'Laval' + '<br>' + 'Titans';
             team_url = 'https://webmountstudio.com/apl_montreal/LT';
         }
         if (team_name === 'Griffintown Warriors') {
@@ -28,21 +30,24 @@ function createTeamInFoElement(teamAPIResponse) {
         if (team_name === 'Parc-Ex Knight Riders') {
             team_url = 'https://webmountstudio.com/apl_montreal/PE';
         }
-        let captain_name = teamAPIResponse[i].captainFName + " " + teamAPIResponse[i].captainLName;
-        let vice_captain_name = teamAPIResponse[i].vcaptainFName + " " + teamAPIResponse[i].vcaptainLName;
+        let captain_name = teamAPIResponse[i].captainFName + "<br> " + teamAPIResponse[i].captainLName;
+        let vice_captain_name = teamAPIResponse[i].vcaptainFName + " <br>" + teamAPIResponse[i].vcaptainLName;
         let playersList = teamAPIResponse[i].players;
         let balance = teamAPIResponse[i].balance;
         let htmlString =
-            '<div class="col-xs-3 col-md-3">\n' +
+            '<div class="col-xs-3 col-md-3" style="border-right: 1px solid #3f4767 ">\n' +
             '<img src="' + team_url + '" width="100px">\n' +
             '<h5 align="center">' + team_name + '</h5>\n' +
             '<table class="table table-striped table-dark">\n' +
             '<thead>\n' +
             '<tr>\n' +
-            '<th scope="col" id="team_captain_row" class="captain">' + captain_name + '(C)</th>\n' +
+            '<th scope="row" id="team_balance_' + team_name + '" style="background-color:#3f4767!important;color:#f9b61a!important;">CAD ' + balance + '</th>\n' +
             '</tr>\n' +
             '</thead>\n' +
             '<tbody>\n' +
+            '<tr>\n' +
+            '<th scope="col" id="team_captain_row" class="captain">' + captain_name + '(C)</th>\n' +
+            '</tr>\n' +
             '<tr>\n' +
             '<th scope="row" id="team_player_' + vice_captain_name + '">' + vice_captain_name + '(VC)</th>\n' +
             '</tr>\n';
@@ -50,22 +55,19 @@ function createTeamInFoElement(teamAPIResponse) {
             let playershtmlString =
                 '<tr>\n' +
                 '<th scope="row" id="team_player_' + playersList[j].playerFName + '_' + playersList[j].playerLName + '">\ ' +
-                '' + playersList[j].playerFName + ' ' + playersList[j].playerLName + '(' + playersList[j].playerPrice + ')</th>\n' +
+                '' + playersList[j].playerFName + '<br> ' + playersList[j].playerLName + '(' + playersList[j].playerPrice + ')</th>\n' +
                 '</tr>\n';
             if (playersList[j].playerPrice === 100) {
                 playershtmlString =
                     '<tr>\n' +
                     '<th scope="row" class="hundered-dollar" id="team_player_' + playersList[j].playerFName + '_' + playersList[j].playerLName + '">\ ' +
-                    '' + playersList[j].playerFName + ' ' + playersList[j].playerLName + '(' + playersList[j].playerPrice + ')</th>\n' +
+                    '' + playersList[j].playerFName + '<br> ' + playersList[j].playerLName + '(' + playersList[j].playerPrice + ')</th>\n' +
                     '</tr>\n';
             }
             htmlString = htmlString + playershtmlString;
         }
 
         let balacehtml =
-            '<tr>\n' +
-            '<th scope="row" id="team_balance_' + team_name + '" style="background-color:black!important;" font-color="white">CAD ' + balance + '</th>\n' +
-            '</tr>\n' +
             '</tbody>\n' +
             '</table>\n' +
             ' </div>';
