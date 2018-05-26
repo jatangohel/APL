a631a047afb255317ea6eda262d2bad27be8d50b
Akshay Shah <akshay.shah@knappily.com>
2018-05-26 03:20:31 -0400

    Dynamic html string generated to be appended as per AJAX response

--- js/main.js
+++ js/main.js
@@ -1,115 +1,119 @@
+let teamAPIResponse;
 
+function createTeamInFoElement(teamAPIResponse) {
+    for (let i = 0; i < teamAPIResponse.length; i++) {
+        let team_name = teamAPIResponse[i].teamName;
+        let captain_name = teamAPIResponse[i].captainFName + " " + teamAPIResponse[i].captainLName;
+        let vice_captain_name = teamAPIResponse[i].vcaptainFName + " " + teamAPIResponse[i].vcaptainLName;
+        let playersList = teamAPIResponse[i].players;
+        let balance = teamAPIResponse.balance;
+        let htmlString =
+            '<div class="col-xs-3 col-md-3">\n' +
+            '<img src="img/Team/Downtown Thunder.jpg" width="100px">\n' +
+            '<h5 align="center">' + team_name + '</h5>\n' +
+            '<table class="table table-striped table-dark">\n' +
+            '<thead>\n' +
+            '<tr>\n' +
+            '<th scope="col" id="team_captain_row" class="captain">' + captain_name + '(C)</th>\n' +
+            '</tr>\n' +
+            '</thead>\n' +
+            '<tbody>\n' +
+            '<tr>\n' +
+            '<th scope="row" id="team_player_' + vice_captain_name + '">' + vice_captain_name + '(VC)</th>\n' +
+            '</tr>\n';
+        for (let j = 0; j < playersList.length; j++) {
+            let playershtmlString =
+                '<tr>\n' +
+                '<th scope="row" id="team_player_' + playersList[j].playerFName + '_' + playersList[j].playerLName + '">\ ' +
+                '' + playersList[j].playerFName + ' ' + playersList[j].playerLName + '(' + playersList[j].playerPrice + ')</th>\n' +
+                '</tr>\n';
+            if (playersList[j].playerPrice === 100) {
+                $('#team_player_' + playersList[j].playerFName + '_' + playersList[j].playerLName + '').addClass("100-dollar-club-player")
+            }
+            htmlString = htmlString + playershtmlString;
+        }
+
+        let balacehtml =
+            '<tr>\n' +
+            '<th scope="row" id="team_balance_'+ team_name +'" style="background-color:black!important;" font-color="white">CAD '+ balance +'</th>\n' +
+            '</tr>\n' +
+            '</tbody>\n' +
+            '</table>\n' +
+            ' </div>';
+
+        htmlString = htmlString + balacehtml;
+        console.log(htmlString);
+        if (i <= 3) {
+            //do nothing
+        }
+        else {
+            //do nothing
+        }
+
+
+    }
+
+}
+
+function displayTeamInfo() {
+    $.ajax({
+        url: "http://cloud-env.m33mn2puip.us-east-2.elasticbeanstalk.com/webapi/teams/getTeams",
+        dataType: "json",
+        crossDomain: true,
+        type: 'GET',
+        contentType: "application/x-www-form-urlencoded; charset=utf-8",
+        success: function (data) {
+            teamAPIResponse = data;
+            console.log('called made');
+            createTeamInFoElement(teamAPIResponse);
+        },
+        error: function (errData) {
+            console.log(errData);
+            alert('error');
+        }
+    });
+}
+
+$(document).ready(function () {
+
+    setTimeout(displayTeamInfo, 5000);
+});
+/*
 function main() {
 
-(function () {
-   'use strict';
+    (function () {
+        'use strict';
 
-  	$('a.page-scroll').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top - 50
-            }, 900);
-            return false;
-          }
-        }
-      });
+        let teamAPIResponse;
+
+        // Portfolio isotope filter
+        $(window).load(function () {
+
+            $.ajax({
+                url: "http://cloud-env.m33mn2puip.us-east-2.elasticbeanstalk.com/webapi/teams/getTeams",
+                dataType: "json",
+                crossDomain: true,
+                type: 'GET',
+                contentType: "application/x-www-form-urlencoded; charset=utf-8",
+                success: function (data) {
+                    teamAPIResponse = data;
 
 
-    // Show Menu on Book
-    $(window).bind('scroll', function() {
-        var navHeight = $(window).height() - 500;
-        if ($(window).scrollTop() > navHeight) {
-            $('.navbar-default').addClass('on');
-        } else {
-            $('.navbar-default').removeClass('on');
-        }
-    });
-
-    $('body').scrollspy({
-        target: '.navbar-default',
-        offset: 80
-    });
-
-	// Hide nav on click
-  $(".navbar-nav li a").click(function (event) {
-    // check if window is small enough so dropdown is created
-    var toggle = $(".navbar-toggle").is(":visible");
-    if (toggle) {
-      $(".navbar-collapse").collapse('hide');
-    }
-  });
-
-  	// Portfolio isotope filter
-    $(window).load(function() {
-        var $container = $('.portfolio-items');
-        $container.isotope({
-            filter: '*',
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
-        });
-        $('.cat a').click(function() {
-            $('.cat .active').removeClass('active');
-            $(this).addClass('active');
-            var selector = $(this).attr('data-filter');
-            $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
+                },
+                error: function (errData) {
+                    console.log(errData);
+                    alert('error');
                 }
             });
-            return false;
-        });
-        $.ajax({
-            url: "http://cloud-env.m33mn2puip.us-east-2.elasticbeanstalk.com/webapi/teams/getTeams",
-            dataType:"jsonp",
-            xhrFields: {
-                withCredentials: true
-            },
-            crossDomain:true,
-            type:'GET',
-            contentType:"application/x-www-form-urlencoded; charset=utf-8",
-            success: function (data) {
-                alert('success');
+            /!* $.get( "http://cloud-env.m33mn2puip.us-east-2.elasticbeanstalk.com/webapi/teams/getTeams", function( data ) {
                 console.log(data);
-            },
-            error: function (errData) {
-                console.log(errData);
-                alert('error');
-            }
-        });
-       /* $.get( "http://cloud-env.m33mn2puip.us-east-2.elasticbeanstalk.com/webapi/teams/getTeams", function( data ) {
-           console.log(data);
-        });*/
+             });*!/
 
-    });
-
-    // Nivo Lightbox
-    $('.portfolio-item a').nivoLightbox({
-            effect: 'slideDown',
-            keyboardNav: true,
         });
 
-	// Testimonial Slider
-	  	$(document).ready(function() {
-	      $("#testimonial").owlCarousel({
-        navigation : false, // Show next and prev buttons
-        slideSpeed : 300,
-        paginationSpeed : 400,
-        singleItem:true
-        });
-
-  	});
-
-}());
+    }());
 
 
 }
-main();
+
+main();*/
