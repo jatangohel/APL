7b98cf0f2b79744bb9287cb80c5ecade1faca7b6
Akshay Shah <akshay.shah@knappily.com>
2018-05-26 14:15:03 -0400

    v1.1

--- js/main.js
+++ js/main.js
@@ -3,13 +3,38 @@ let teamAPIResponse;
 function createTeamInFoElement(teamAPIResponse) {
     for (let i = 0; i < teamAPIResponse.length; i++) {
         let team_name = teamAPIResponse[i].teamName;
+        let team_url;
+        if (team_name === 'NDG Strikers') {
+            team_url = 'https://webmountstudio.com/apl_montreal/NDG';
+        }
+        if (team_name === 'Mont-Royal Eagles') {
+            team_url = 'https://webmountstudio.com/apl_montreal/MR';
+        }
+        if (team_name === 'West Island Mustangs') {
+            team_url = 'https://webmountstudio.com/apl_montreal/WI';
+        }
+        if (team_name === 'Downtown Thunder') {
+            team_url = 'https://webmountstudio.com/apl_montreal/DT';
+        }
+        if (team_name === 'South-Shore Lions') {
+            team_url = 'https://webmountstudio.com/apl_montreal/SS';
+        }
+        if (team_name === 'Laval Titans') {
+            team_url = 'https://webmountstudio.com/apl_montreal/LT';
+        }
+        if (team_name === 'Griffintown Warriors') {
+            team_url = 'https://webmountstudio.com/apl_montreal/GW';
+        }
+        if (team_name === 'Parc-Ex Knight Riders') {
+            team_url = 'https://webmountstudio.com/apl_montreal/PE';
+        }
         let captain_name = teamAPIResponse[i].captainFName + " " + teamAPIResponse[i].captainLName;
         let vice_captain_name = teamAPIResponse[i].vcaptainFName + " " + teamAPIResponse[i].vcaptainLName;
         let playersList = teamAPIResponse[i].players;
-        let balance = teamAPIResponse.balance;
+        let balance = teamAPIResponse[i].balance;
         let htmlString =
             '<div class="col-xs-3 col-md-3">\n' +
-            '<img src="img/Team/Downtown Thunder.jpg" width="100px">\n' +
+            '<img src="' + team_url + '" width="100px">\n' +
             '<h5 align="center">' + team_name + '</h5>\n' +
             '<table class="table table-striped table-dark">\n' +
             '<thead>\n' +
@@ -28,25 +53,31 @@ function createTeamInFoElement(teamAPIResponse) {
                 '' + playersList[j].playerFName + ' ' + playersList[j].playerLName + '(' + playersList[j].playerPrice + ')</th>\n' +
                 '</tr>\n';
             if (playersList[j].playerPrice === 100) {
-                $('#team_player_' + playersList[j].playerFName + '_' + playersList[j].playerLName + '').addClass("100-dollar-club-player")
+                playershtmlString =
+                    '<tr>\n' +
+                    '<th scope="row" class="hundered-dollar" id="team_player_' + playersList[j].playerFName + '_' + playersList[j].playerLName + '">\ ' +
+                    '' + playersList[j].playerFName + ' ' + playersList[j].playerLName + '(' + playersList[j].playerPrice + ')</th>\n' +
+                    '</tr>\n';
             }
             htmlString = htmlString + playershtmlString;
         }
 
         let balacehtml =
             '<tr>\n' +
-            '<th scope="row" id="team_balance_'+ team_name +'" style="background-color:black!important;" font-color="white">CAD '+ balance +'</th>\n' +
+            '<th scope="row" id="team_balance_' + team_name + '" style="background-color:black!important;" font-color="white">CAD ' + balance + '</th>\n' +
             '</tr>\n' +
             '</tbody>\n' +
             '</table>\n' +
             ' </div>';
 
         htmlString = htmlString + balacehtml;
-        console.log(htmlString);
+        //console.log(htmlString);
         if (i <= 3) {
+            $('#first_4_teams').append(htmlString);
             //do nothing
         }
         else {
+            $('#last_4_teams').append(htmlString);
             //do nothing
         }
 
@@ -76,7 +107,7 @@ function displayTeamInfo() {
 
 $(document).ready(function () {
 
-    setTimeout(displayTeamInfo, 5000);
+    displayTeamInfo();
 });
 /*
 function main() {
